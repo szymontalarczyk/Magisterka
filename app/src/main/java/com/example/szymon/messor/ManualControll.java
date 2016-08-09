7bcd32bdf04bf2adb3386367e12c0aaa51e461fc
szymon talarczyk <xtalar@gmail.com>
2016-08-09 22:21:23 +0200

    dodano spinner

--- app/src/main/java/com/example/szymon/messor/ManualControll.java
+++ app/src/main/java/com/example/szymon/messor/ManualControll.java
@@ -6,18 +6,40 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by pawel on 21.04.16.
  */
-public class ManualControll extends Fragment{
+public class ManualControll extends Fragment implements AdapterView.OnItemSelectedListener{
 
     View myView;
-
+    Spinner spinner;
+    ArrayAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.manualcontroll, container, false);
+        spinner = (Spinner)myView.findViewById(R.id.spinner);
+adapter = ArrayAdapter.createFromResource(this.getActivity(),R.array.lista_komend,android.R.layout.simple_spinner_item);
+       spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         return myView;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+        TextView selected_option = (TextView) view;
+        Toast.makeText(this.getActivity(),"Wybrano: "+selected_option.getText(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
